b5fe374e99e7e237821d2d2f14ec9c6a53c3d29b
malekannabi <malekannabi5@gmail.com>
2021-08-04 16:54:16 +0200

    rest of portfolio

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -1,25 +1,25 @@
-import "./menu.scss"
+import "./menu.scss";
 
-export default function Menu({menuOpen,setMenuOpen}) {
-    return (
-        <div className={"menu "+(menuOpen && "active")}>
-            <ul>
-                <li>
-                    <a href="#intro">Home</a>
-                </li>
-                <li>
-                    <a href="#portfolio">Portfolio</a>
-                </li>
-                <li>
-                    <a href="#works">Works</a>
-                </li>
-                <li>
-                    <a href="#testimonials">Testimonials</a>
-                </li>
-                <li>
-                    <a href="#contact">Contact</a>
-                </li>
-            </ul>
-        </div>
-    )
+export default function Menu({ menuOpen, setMenuOpen }) {
+  return (
+    <div className={"menu "+(menuOpen && "active")}>
+      <ul>
+        <li onClick={()=>setMenuOpen(false)}>
+          <a href="#intro">Home</a>
+        </li>
+        <li onClick={()=>setMenuOpen(false)}>
+          <a href="#portfolio">Portfolio</a>
+        </li>
+        <li onClick={()=>setMenuOpen(false)}>
+          <a href="#works">Works</a>
+        </li>
+        <li onClick={()=>setMenuOpen(false)}>
+          <a href="#testimonials">Testimonials</a>
+        </li>
+        <li onClick={()=>setMenuOpen(false)}>
+          <a href="#contact">Contact</a>
+        </li>
+      </ul>
+    </div>
+  );
 }
