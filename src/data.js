6935534fd8a5d2c76670d70e76d40730b397ba58
malekannabi <malekannabi5@gmail.com>
2021-08-07 14:10:33 +0200

    small changes

--- src/data.js
+++ src/data.js
@@ -2,42 +2,42 @@ export const featuredPortfolio = [
     {
       id: 1,
       title: "Social Media App",
-      content:"this is just a test",
+      content:"Coming Soon !",
       img:
         "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKvdHn8GmPGCO0y3SJqNHACygpm0h9VycMHg&usqp=CAU",
     },
     {
       id: 2,
       title: "Rampa UI Design",
-      content:"this is just a test",
+      content:"Coming Soon !",
       img:
         "https://cdn.dribbble.com/users/702789/screenshots/15054318/media/4ea5d492b7b07eebc9528ff960794879.png?compress=1&resize=1200x900",
     },
     {
       id: 3,
       title: "E-commerce Web Design",
-      content:"this is just a test",
+      content:"Coming Soon !",
       img:
         "https://cdn.dribbble.com/users/1387827/screenshots/15466426/media/deb2dca6762cd3610321c98bfccb0b72.png?compress=1&resize=1200x900",
     },
     {
       id: 4,
       title: "Relax Mobile App",
-      content:"this is just a test",
+      content:"Coming Soon !",
       img:
         "https://cdn.dribbble.com/users/4095861/screenshots/15467417/media/d6a15c416626f12b31fa5ca1db192572.png?compress=1&resize=1200x900",
     },
     {
       id: 5,
       title: "Hero Web Design",
-      content:"this is just a test",
+      content:"Coming Soon !",
       img:
         "https://cdn.dribbble.com/users/5031392/screenshots/15467520/media/c36b3b15b25b1e190d081abdbbf947cf.png?compress=1&resize=1200x900",
     },
     {
       id: 6,
       title: "Banking App",
-      content:"this is just a test",
+      content:"Coming Soon !",
       img:
         "https://cdn.dribbble.com/users/3307260/screenshots/15468444/media/447b4501f7a145b05e11c70199a9c417.jpg?compress=1&resize=1200x900",
     },
@@ -47,25 +47,25 @@ export const featuredPortfolio = [
     {
       id: 1,
       title: "Web Blog App",
-      content:"this is just a test",
+      content:"Coming Soon !",
       img: "assets/blog.png",
     },
     {
       id: 2,
       title: "Web Content Creator App",
-      content:"this is just a test",
+      content:"Coming Soon !",
       img: "assets/ContentCreator.png",
     },
     {
       id: 3,
       title: "Web E-commerce App",
-      content:"this is just a test",
+      content:"Coming Soon !",
       img: "assets/Ecomerce.png",
     },
     {
       id: 4,
       title: "Web Management Dashboard App",
-      content:"this is just a test",
+      content:"Coming Soon !",
       img: "assets/fooddelivery.png",
     },
   ];
@@ -74,42 +74,42 @@ export const featuredPortfolio = [
     {
       id: 1,
       title: "Mobile Social Media App",
-      content:"this is just a test",
+      content:"Coming Soon !",
       img:
         "https://cdn.dribbble.com/users/2597268/screenshots/15468893/media/44313553d73ba41580f1df69749bba28.jpg?compress=1&resize=1200x900",
     },
     {
       id: 2,
       title: "Mobile Rampa UI Design",
-      content:"this is just a test",
+      content:"Coming Soon !",
       img:
         "https://cdn.dribbble.com/users/5920881/screenshots/15463142/media/b5a460223798bd85d835710806e2f3dd.png?compress=1&resize=1200x900",
     },
     {
       id: 3,
       title: "Mobile E-commerce Design",
-      content:"this is just a test",
+      content:"Coming Soon !",
       img:
         "https://cdn.dribbble.com/users/1998175/screenshots/15459384/media/48ac2b43ebe81ba0866afea1383cc939.png?compress=1&resize=1200x900",
     },
     {
       id: 4,
       title: "Mobile Relax App",
-      content:"this is just a test",
+      content:"Coming Soon !",
       img:
         "https://cdn.dribbble.com/users/2083704/screenshots/15468619/media/cd958306c7a772449e1ac23bd65ce506.png?compress=1&resize=1200x900",
     },
     {
       id: 5,
       title: "Mobile Hero Design",
-      content:"this is just a test",
+      content:"Coming Soon !",
       img:
         "https://cdn.dribbble.com/users/26642/screenshots/15462545/media/1a202ef5c8338b6f78634e6edc1ba350.png?compress=1&resize=1200x900",
     },
     {
       id: 6,
       title: "Mobile Banking App",
-      content:"this is just a test",
+      content:"Coming Soon !",
       img:
         "https://cdn.dribbble.com/users/1387827/screenshots/15466426/media/deb2dca6762cd3610321c98bfccb0b72.png?compress=1&resize=1200x900",
     },
@@ -119,42 +119,42 @@ export const featuredPortfolio = [
     {
       id: 1,
       title: "Design Social Media App",
-      content:"this is just a test",
+      content:"Coming Soon !",
       img:
         "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKvdHn8GmPGCO0y3SJqNHACygpm0h9VycMHg&usqp=CAU",
     },
     {
       id: 2,
       title: "Design Rampa UI Design",
-      content:"this is just a test",
+      content:"Coming Soon !",
       img:
         "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKvdHn8GmPGCO0y3SJqNHACygpm0h9VycMHg&usqp=CAU",
     },
     {
       id: 3,
       title: "Design E-commerce Web Design",
-      content:"this is just a test",
+      content:"Coming Soon !",
       img:
         "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKvdHn8GmPGCO0y3SJqNHACygpm0h9VycMHg&usqp=CAU",
     },
     {
       id: 4,
       title: "Design Relax Mobile App",
-      content:"this is just a test",
+      content:"Coming Soon !",
       img:
         "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKvdHn8GmPGCO0y3SJqNHACygpm0h9VycMHg&usqp=CAU",
     },
     {
       id: 5,
       title: "Design Keser Web Design",
-      content:"this is just a test",
+      content:"Coming Soon !",
       img:
         "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKvdHn8GmPGCO0y3SJqNHACygpm0h9VycMHg&usqp=CAU",
     },
     {
       id: 6,
       title: "Design Banking App",
-      content:"this is just a test",
+      content:"Coming Soon !",
       img:
         "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKvdHn8GmPGCO0y3SJqNHACygpm0h9VycMHg&usqp=CAU",
     },
@@ -164,42 +164,42 @@ export const featuredPortfolio = [
     {
       id: 1,
       title: "Content Social Media App",
-      content:"this is just a test",
+      content:"Coming Soon !",
       img:
         "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKvdHn8GmPGCO0y3SJqNHACygpm0h9VycMHg&usqp=CAU",
     },
     {
       id: 2,
       title: "Content Rampa UI Design",
-      content:"this is just a test",
+      content:"Coming Soon !",
       img:
         "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKvdHn8GmPGCO0y3SJqNHACygpm0h9VycMHg&usqp=CAU",
     },
     {
       id: 3,
       title: "Content E-commerce Web Design",
-      content:"this is just a test",
+      content:"Coming Soon !",
       img:
         "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKvdHn8GmPGCO0y3SJqNHACygpm0h9VycMHg&usqp=CAU",
     },
     {
       id: 4,
       title: "Content Relax Mobile App",
-      content:"this is just a test",
+      content:"Coming Soon !",
       img:
         "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKvdHn8GmPGCO0y3SJqNHACygpm0h9VycMHg&usqp=CAU",
     },
     {
       id: 5,
       title: "Content Keser Web Design",
-      content:"this is just a test",
+      content:"Coming Soon !",
       img:
         "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKvdHn8GmPGCO0y3SJqNHACygpm0h9VycMHg&usqp=CAU",
     },
     {
       id: 6,
       title: "Content Banking App",
-      content:"this is just a test",
+      content:"Coming Soon !",
       img:
         "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKvdHn8GmPGCO0y3SJqNHACygpm0h9VycMHg&usqp=CAU",
     },
